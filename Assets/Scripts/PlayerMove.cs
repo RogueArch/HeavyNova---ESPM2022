c820d83cd4e954ead93c23ce11462dbc898aa91b
Caio <caiomendes2704@gmail.com>
2022-04-08 22:36:25 -0300

    Tiro + barris

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public CharacterController characterController;
+    Vector3 move;
+    public float speed = 2;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        characterController.SimpleMove(move* speed);
+    }
+
+    void FixedUpdate()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            Debug.DrawLine(ray.origin, hit.point);
+            Vector3 dir =  hit.point - transform.position;
+            dir = new Vector3(dir.x, 0, dir.z);
+            transform.forward = dir;
+        }
+            
+    }
+}
