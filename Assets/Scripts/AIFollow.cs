f8713e5a9dca125f52648faf09d743bf2e7546d5
Caio <caiomendes2704@gmail.com>
2022-05-06 22:23:35 -0300

    aranhas e explosão

---
+++ Assets/Scripts/AIFollow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIFollow : MonoBehaviour
+{
+    public GameObject player;
+    public UnityEngine.AI.NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+}
